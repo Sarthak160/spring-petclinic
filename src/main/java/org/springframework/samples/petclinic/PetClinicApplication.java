554bc5741ef1a45ab1a600df8a8119f6fefc63f6
Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>
2022-12-20 15:39:27 +0530

    feat(test): add keploy integration for generating testcases via api calls
    Signed-off-by: Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,8 +16,10 @@
 
 package org.springframework.samples.petclinic;
 
+import io.keploy.servlet.KeployMiddleware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * PetClinic Spring Boot Application.
@@ -26,6 +28,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@Import(KeployMiddleware.class)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
